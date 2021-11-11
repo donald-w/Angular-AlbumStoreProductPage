e4a5ad9414282fd1f5970ecd7a21237e9b0d4ae5
Donald Walker <github@donaldw.com>
2021-11-11 23:41:27 +0000

    keep random whitespace checker happer

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs';
+
 import 'rxjs/add/operator/map';
 
 @Injectable()
-
 export class ProductService {
-  private _albumUrl: string = '../assets/album.json';
+
+  private _albumUrl = '../assets/album.json';
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) : Observable<any> {
-    return this._http.get(this._albumUrl).map(response => response.json());
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map((response) => response.json());
   }
-
 }
